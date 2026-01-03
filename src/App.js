d4a0d8196de3628d0d9d060ce03d9702f28bc67c
Haoxiang Xu <haoxiang-xu>
2026-01-03 15:40:24 -0800

    default setting update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ConfigContainer from "./CONTAINERs/config/container";
 import DemoPage from "./PAGEs/demo";
 import IconDemo from "./DEMOs/icon_demo/icon_demo";
 import SwitchDemo from "./DEMOs/switch_demo/switch_demo";
+import DndDemo from "./DEMOs/dnd_demo/dnd_demo";
 /* { Demos ( remove during production ) } -------------------------------------------------------------------------------------- */
 
 const App = () => {
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/mini" element={<DemoPage />} />
           <Route path="/mini/icon" element={<IconDemo />} />
           <Route path="/mini/switch" element={<SwitchDemo />} />
+          <Route path="/mini/dnd" element={<DndDemo />} />
           {/* { Demos ( remove during production ) } --------------------------------------------------------------------------- */}
         </Routes>
       </Router>
